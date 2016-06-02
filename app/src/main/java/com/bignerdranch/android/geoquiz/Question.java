40b2ace9cf2a0371a6ee0073c04c6477ae8f2d84
winslydevN <winslydev@gmail.com>
2016-06-02 20:50:11 +0300

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        this.mTextResId = textResId;
+        this.mAnswerTrue = answerTrue;
+    }
+}
